80eb31c51d21c0b4a3a599f09dd15320d5f8d740
Leonardo Alexandre <32283837+leoafs@users.noreply.github.com>
2022-05-12 02:26:02 -0300

    Add files via upload

--- s.py
+++ s.py
@@ -1,78 +1,73 @@
-from __future__ import division
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 import time
 
-year = 0
-day = 0
 
-def init(): 
-   glClearColor (0.0, 0.0, 0.0, 0.0)
-   glShadeModel (GL_FLAT)
+year=0
+day=0
 
-
-import time
-
+paused = False 
 start_time = time.time()
 
 def display():
 
-    t = time.time() - start_time
-    year_period = 120.0                  # 120 seconds for simulating one year 
-    year     = (t / year_period)
-    day      = 365 * year
-    moon_sid = (365 / 27.3) * year
+   
+    global t,year_period, year, day
+
+    if not paused:
+         t = time.time() - start_time
+         year_period = 120.0                  # 120 seconds for simulating one year 
+         year = (t / year_period)
+         day  = 365 * year
 
     glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT )
     glEnable( GL_DEPTH_TEST )
 
     #SOL
-    glColor4f (1.0, 1.0, 0, 1)
-    #glRotatef(0.1, 0.0, 1.0, 0.0)     
-    glutSolidSphere(1.0, 20, 16)
+    glColor4f (1.0, 1.0, 0, 0)
+    #glRotatef(90, 0.0, 0.0, 0.0)     
+    glutSolidSphere(1, 20, 20)
+    #glutWireSphere(1.0, 20, 20)
     glPushMatrix()
 
     #Planeta1 
-    glRotatef(year*360.0, 0.0, 1.0, 0.0)     # earth rotation around the sun 
-    glTranslatef(2.0, 0.0, 0.0)              # earth location
-              
-    glPushMatrix()                           # push earth system               
-    glRotatef(day*360.0, 1.0, 0.0, 0.0)      # earth spinn
-    glRotatef(90-23.4, 0.0, 1.0, 0.0)        # earth axis
-    glColor3f (0, 0, 1)                      # blue
-    glutWireSphere(0.3, 10, 8)               
-    glutSolidSphere(0.3, 10, 8)             
+    glRotatef(year*360.0, 0.0, 1.0, 0.0)     # Rotação em torno do sol 
+    glTranslatef(3.0, 0.0, 0.0)              # posição do planeta 1 em relação ao sol
+    glPushMatrix()                           # push               
+    glColor3f (0, 0, 1)                      # Azul
+    glutSolidSphere(0.3, 12, 12)
     glPopMatrix()
+
+    
     #LUA1
     glPushMatrix()
-    glRotatef(moon_sid*360.0, 0.0, 1.0, 0.0) # moon sidereal
-    glTranslatef(1.0, 0.0, 0.0)              # distance moon to earth
-    glRotatef(90, 1.0, 0.0, 0.0)
-    glColor4f (0.4, 0.5, 0.6, 1)                         
-    glutWireSphere(0.1, 10, 8)               # moon
+    glRotatef(day*45, 0.0, 1.0, 0.0)         # translação da lua 1 em torno do planeta 1
+    glTranslatef(0.45, 0.0, 0.0)              # deslocamento da lua 1 em relação ao planeta
+    glColor3f (0.4, 0.5, 0.6)                         
+    glutSolidSphere(0.08, 10,10)              # LUA 1
+    
     glPopMatrix()
 
     #LUA2
     glPushMatrix()
-    glRotatef(moon_sid*360.0, 0.0, 1.0, 1.0) # moon sidereal
-    glTranslatef(0.0, 1.0, 0.0)              # distance moon to earth
-    glRotatef(90, 0.0, 1.0, 0.0)
-    glColor4f (0.4, 0.5, 0.6, 1)                         
-    glutWireSphere(0.1, 10, 8)               # moon
+    glRotatef(day*24, 0.5,0.5, 0.5)          # translação da lua 2 em torno do planeta 2
+    glTranslatef(0.0, 0.5, -0.5)            #  deslocamento da lua 2 em relação ao planeta
+    glColor3f (0.4, 0.5, 0.6)                       
+    glutSolidSphere(0.08, 10, 10)              # LUA 2
     glPopMatrix()
 
     glPopMatrix()      
 
     #Planeta2
     glPushMatrix()
-    glRotatef((-1)*year*360.0, 0.0, 1.0, 0.0)     # earth rotation around the sun 
-    glTranslatef(-3.0, 0.0, 0.0)              # earth location
-    glRotatef(day*360.0, 0.0, 1.0, 0.0)      # earth spinn
-    glRotatef(90-23.4, 90-23.4, 0.0, 0.0)        # earth axis
-    glColor3f (1, 0, 0)                      
-    glutWireSphere(0.3, 20, 8)               # earth
-    #glutSolidSphere(0.3, 12, 8) 
+    glRotatef((-1)*year*360.0, 0.0, 1.0, 0.0)     # planeta 2 em torno do sol
+    glTranslatef(4.0, 0.0, 0.0)              
+    #glRotatef(day*45, 0.0, 1.0, 0.0)              
+    glRotatef(90, 1, 0, 0)        
+    glColor3f (1, 0, 0)                           #Vermelho    
+    #glutWireSphere(0.3, 10, 8)               
+    glutSolidSphere(0.3, 12, 12)                  #Planeta 2
     glPopMatrix()
     
     glutPostRedisplay()
@@ -85,15 +80,27 @@ def reshape(w, h):
    gluPerspective(70.0, w/h, 1.0, 20.0)
    glMatrixMode(GL_MODELVIEW)
    glLoadIdentity()
-   gluLookAt (0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+   gluLookAt (0.0, 0.0, 8.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
 
+    
+def Y(*args):
+
+    global paused
+    if args[0] == b'y':
+        paused = not paused
+        
+    glutPostRedisplay()
+
+
+    
 glutInit()
 glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
-glutInitWindowSize(800, 800)
+glutInitWindowSize(900, 800)
 glutInitWindowPosition (100, 100)
-glutCreateWindow("Transformation")
-init ()
-glutDisplayFunc(display)
+glutCreateWindow("Leonardo")
 glutIdleFunc(display)
+glutKeyboardFunc(Y)
+glutDisplayFunc(display)
 glutReshapeFunc(reshape)
+
 glutMainLoop()
